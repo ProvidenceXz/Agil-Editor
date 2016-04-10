eaea67d9e7649e1c13ce6d6946cd8b883e0f6c7f
ProvidenceXz <zhangxian1942@live.com>
2016-04-09 23:05:32 -0700

    Initial commit

---
+++ src/Editor.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.geometry.VPos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+/* Understands how to display text through GUI in a reasonable manner. */
+public class Editor extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
---
+++ test/EditorTest.java
@@ -0,0 +1,19 @@
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+
+/**
+ * Created by ProvidenceXz on 4/2/2016.
+ */
+public class EditorTest {
+
+    @Before
+    public void initialize() {
+        Editor.launch();
+    }
+
+}
